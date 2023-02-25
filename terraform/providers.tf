fa6b2ba5d23adf4f1a27e094d168d7b10ca17940
Mustafa Elhendy <58703269+elhendymustafa7@users.noreply.github.com>
2023-02-25 19:00:26 +0200

    Delete terraform directory

--- terraform/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.region
-
-  shared_config_files      = ["/home/mustafa/.aws/config"]
-  shared_credentials_files = ["/home/mustafa/.aws/credentials"]
-
-
-  # other options for authentication
-}
--- terraform/terraform.tfvars
+++
@@ -1,8 +0,0 @@
-
-region                   = "us-west-2"
-availability_zones_count = 2
-
-project = "ITI-final-Project"
-
-vpc_cidr         = "10.0.0.0/16"
-subnet_cidr_bits = 8
